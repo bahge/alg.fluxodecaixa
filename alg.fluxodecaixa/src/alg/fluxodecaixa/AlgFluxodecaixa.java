c15f6e2b1b0d7742a73d1d8b4cec9f57d651aafb
BaHGe <leandrobage@gmail.com>
2019-08-26 20:08:38 -0400

    Commit alteração 2

--- alg.fluxodecaixa/src/alg/fluxodecaixa/AlgFluxodecaixa.java
+++ alg.fluxodecaixa/src/alg/fluxodecaixa/AlgFluxodecaixa.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package alg.fluxodecaixa;
 
-/**
- *
- * @author bahge
- */
 public class AlgFluxodecaixa {
 
     /**
